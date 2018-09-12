b685cf07be97db4a9f08a9889dbed0fed4787614
Scott Rock <scottrockbiz@gmail.com>
2018-09-11 17:18:09 -0700

    bar code generator app working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'barcode-app';
+  title: string = 'Retro Bar Code';
+  bars: Array<string> = [];
+
+  createBars: Function = function() {
+    const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'purple'];
+    for ( let idx = 0; idx < 10; idx += 1 ) {
+      this.bars[idx] = colors[Math.floor(Math.random() * 6)];
+    }
+  }
+
+  ngOnInit() {
+    this.createBars();
+  }
+
 }
